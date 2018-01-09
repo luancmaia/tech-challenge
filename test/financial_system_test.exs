33a413b8a022a4626f7f2de7f3f29d99c00bdbf1
Gabriel Godoy <gabriel.oliveira@stone.com.br>
2018-01-09 18:47:44 -0200

    fix: break all tests by default

--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -3,20 +3,26 @@ defmodule FinancialSystemTest do
   doctest FinancialSystem
 
   test "User should be able to transfer money to another account" do
+    assert :false
   end
 
   test "User cannot transfer if not enough money available on the account" do
+    assert :false
   end
 
   test "A transfer should be cancelled if an error occurs" do
+    assert :false
   end
 
   test "A transfer can be splitted between 2 or more accounts" do
+    assert :false
   end
 
   test "User should be able to exchange money between different currencies" do
+    assert :false
   end
 
   test "Currencies should be in compliance with ISO 4217" do
+    assert :false
   end
 end
